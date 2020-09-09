06d4b722ffc6064eceb5d82c602b0cdbb9b2114b
unknown <viniciusbeloti7@gmail.com>
2020-09-09 11:02:17 -0300

    feat: add Sidebar

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./styles.css";
+
+const Sidebar: React.FC = () => {
+  return (
+    <div className="sidebar">
+      <h1>Sidebar</h1>
+    </div>
+  );
+};
+
+export default Sidebar;
