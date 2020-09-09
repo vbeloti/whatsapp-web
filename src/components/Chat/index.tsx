a17a26663a00b85c51304cf5cf577991a2b61974
unknown <viniciusbeloti7@gmail.com>
2020-09-09 11:03:29 -0300

    feat: add Chat

---
+++ src/components/Chat/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./styles.css";
+
+const Chat: React.FC = () => {
+  return (
+    <div className="chat">
+      <h1>Chat</h1>
+    </div>
+  );
+};
+
+export default Chat;
