cb3cf975e6a9810eca88c3c8a1b0884c433393e0
unknown <viniciusbeloti7@gmail.com>
2020-09-09 15:40:35 -0300

    feat: add image project

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
-import React from "react";
-import "./App.css";
-import Sidebar from "./components/Sidebar";
-import Chat from "./components/Chat";
+import React, { useEffect } from 'react';
+import Pusher from 'pusher-js';
+import * as dontenv from 'dotenv';
+import './App.css';
+import Sidebar from './components/Sidebar';
+import Chat from './components/Chat';
+
+dontenv.config();
+
+interface IData {
+  name: string;
+  message: string;
+}
 
 function App() {
+  useEffect(() => {
+    const pusher = new Pusher('3277987e3192daa36880', {
+      cluster: 'eu',
+    });
+
+    const channel = pusher.subscribe('my-channel');
+
+    channel.bind('my-event', function(data: IData) {
+      alert(JSON.stringify(data));
+    });
+  }, []);
+
   return (
     <div className="app">
       <div className="app__body">
