4724dc0ea97b3f4db252a36f0bda7176d4e94d2e
unknown <viniciusbeloti7@gmail.com>
2020-09-09 16:33:39 -0300

    chore: add unbind to App useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,30 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Pusher from 'pusher-js';
 import * as dontenv from 'dotenv';
 import './App.css';
 import Sidebar from './components/Sidebar';
 import Chat from './components/Chat';
+import Axios from 'axios';
+import api from './services/api';
 
 dontenv.config();
 
 interface IData {
   name: string;
   message: string;
+  timestamp: string;
 }
 
 function App() {
+  const [messages, setMessages] = useState<IData[]>([]);
+
+  useEffect(() => {
+    api.get('/api/v1/messages').then((response) => {
+      setMessages(response.data);
+    });
+
+  }, []);
+
   useEffect(() => {
     const pusher = new Pusher('3277987e3192daa36880', {
       cluster: 'eu',
@@ -20,11 +32,18 @@ function App() {
 
     const channel = pusher.subscribe('messages');
 
-    channel.bind('inserted', function(data: IData) {
-      alert(JSON.stringify(data));
+    channel.bind('inserted', function (newMessage: IData) {
+      setMessages([...messages, newMessage]);
     });
 
-  }, []);
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+    };
+
+  }, [messages]);
+
+  console.log(messages);
 
   return (
     <div className="app">
