6f8dd0697238763ce34a7a03ba6aacb34134c87a
unknown <viniciusbeloti7@gmail.com>
2020-09-09 11:31:14 -0300

    feat: add SidebarChat

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -3,7 +3,10 @@ import React from 'react';
 import ChatIcon from '@material-ui/icons/Chat';
 import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
 import { Avatar, IconButton } from '@material-ui/core';
+
+import avatar from '../../assets/img/avatar.svg';
 
 import './styles.css';
 
@@ -11,6 +14,7 @@ const Sidebar: React.FC = () => {
   return (
     <div className="sidebar">
       <div className="sidebar__header">
+        <Avatar src={avatar} />
         <div className="sidebar__headerRight">
           <IconButton>
             <DonutLargeIcon />
@@ -21,6 +25,13 @@ const Sidebar: React.FC = () => {
           <IconButton>
             <MoreVertIcon />
           </IconButton>
+        </div>
+      </div>
+
+      <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
+          <SearchOutlinedIcon />
+          <input placeholder="Procurar ou começar uma nova conversa" type="text"/>
         </div>
       </div>
     </div>
---
+++ src/components/SidebarChat/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles.css';
+
+const SidebarChat: React.FC = () => {
+  return (
+    <div className="sidebarChat">
+
+    </div>
+  );
+}
+
+export default SidebarChat;
